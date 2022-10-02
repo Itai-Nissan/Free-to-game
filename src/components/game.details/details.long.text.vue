98c0941cfa605db2a05a7a3663f60edb04cbd9bc
Itai Nissan <itainissan@gmail.com>
2022-10-02 17:44:47 +0300

    updated details about and system req

---
+++ src/components/game.details/details.long.text.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="details-long-text">
+        <p>{{formatedText}}<span v-if="!isMore && longText">...</span>
+        </p>
+        <span :style="readStyle" v-if="longText" @click="isMore=!isMore">{{readText}} </span>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'details-long-text',
+    props: {
+        text: String
+    },
+    data() {
+        return {
+            isMore: false,
+            longText: this.text.length > 500,
+        }
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {},
+    computed: {
+        formatedText() {
+            return this.isMore ? this.text : this.text.slice(0, 500)
+        },
+        readText() {
+            return this.isMore ? ' - Read Less' : ' + Read more'
+        },
+        readStyle() {
+            return {
+                color: this.isMore ? '#696969' : '#d4d3d3',
+                cursor: 'pointer',
+            }
+        },
+    },
+}
+</script>
--- src/components/game.details/game.details.info.vue
+++ src/components/game.details/game.details.info.vue
@@ -1,13 +1,12 @@
 <template>
     <section v-if="game" class="game-details-info">
-        <!-- <pre>{{game}}</pre> -->
         <section class="game-info">
             <h1>{{game.title}}</h1>
         </section>
         <section class="game-about">
             <h2>About {{game.title}}</h2>
             <hr>
-            <p>{{game.description}}</p>
+            <details-long-text :text="game.description" />
         </section>
         <section class="game-additional">
             <div class="header">
@@ -15,21 +14,44 @@
                 <hr>
             </div>
             <div class="additional-table">
-                <span class="text-muted">Title <br> {{game.title}}</span>
-                <span class="text-muted">Developer <br> {{game.developer}}</span>
-                <span class="text-muted">Publisher <br> {{game.publisher}}</span>
+                <div class="table-text">
+                    <span class="">Title</span>
+                    <br>
+                    <span class="">{{game.title}}</span>
+                </div>
+                <div class="table-text">
+                    <span class="">Developer</span>
+                    <br>
+                    <span class="">{{game.developer}}</span>
+                </div>
+                <div class="table-text">
+                    <span class="">Publisher</span>
+                    <br>
+                    <span class="">{{game.publisher}}</span>
+                </div>
             </div>
             <div class="additional-table">
-                <span class="text-muted">Release Date <br> {{game.release_date}}</span>
-                <span class="text-muted">Genre <br> {{game.genre}}</span>
-                <span class="text-muted">Platform <br> {{game.platform}}</span>
+                <div class="table-text">
+                    <span class="">Release Date</span>
+                    <br>
+                    <span class="">{{game.release_date}}</span>
+                </div>
+                <div class="table-text">
+                    <span class="">Genre</span>
+                    <br>
+                    <span class="">{{game.genre}}</span>
+                </div>
+                <div class="table-text">
+                    <span class="">Platform</span>
+                    <br>
+                    <span class="">{{game.platform}}</span>
+                </div>
             </div>
         </section>
         <section v-if="game.screenshots" class="screenshots">
             <h2>{{game.title}} Screenshots</h2>
             <hr>
             <div class="images">
-                <!-- <a :href="game.screenshots[0].image">testing</a> -->
                 <a :href="game.screenshots[0].image">
                     <img v-bind:src="game.screenshots[0].image" alt="">
                 </a>
@@ -41,22 +63,38 @@
                 </a>
             </div>
         </section>
-        <!-- <img v-bind:src="game.thumbnail" alt=""> -->
-        <!-- <h5>{{game.short_description}}</h5> -->
-        <!-- <h5>Genre: {{game.genre}}</h5> -->
-        <!-- <h5>By: {{game.publisher}}</h5> -->
-        <!-- <a :href="game.game_url">{{game.game_url}}</a> -->
+        <section class="system-req">
+            <div v-for="(gameReq, idx) in game.minimum_system_requirements">
+                <div class="additional-table">
+                    <div class="table-text">
+                        <span>{{idx}}</span>
+                        <br>
+                        <span>{{gameReq}}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="additional-table">
+                <div class="table-text">
+                    <span>Additional Notes</span>
+                    <br>
+                    <span>Specifications may change during development</span>
+                </div>
+            </div>
+        </section>
     </section>
     <div v-else class="loading"></div>
 </template>
     
 <script>
+import detailsLongText from './details.long.text.vue'
+
 export default {
     name: 'game-details-info',
     props: {
         game: Object
     },
     components: {
+        detailsLongText,
     },
     data() {
         return {
