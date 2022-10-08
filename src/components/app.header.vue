369ee463f2cea1a60e4561403b992a41c2a9898d
Itai Nissan <itainissan@gmail.com>
2022-10-08 11:30:26 +0300

    sticky header and details back img

--- src/components/app.header.vue
+++ src/components/app.header.vue
@@ -1,15 +1,17 @@
 <template>
-  <section id="sticky" :class="{fixed: isFixed}" class="app-header container">
-    <div class="logo">
-      <RouterLink to="/">
-        <img src="../assets/img/freetogame-logo.png" alt="">
-      </RouterLink>
+  <section id="sticky" :class="{fixed: isFixed}" class="app-header">
+    <div class="header-content container">
+      <div class="logo">
+        <RouterLink to="/">
+          <img src="../assets/img/freetogame-logo.png" alt="">
+        </RouterLink>
+      </div>
+      <nav class="nav">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/game">Games</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
-    <nav class="nav">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/game">Games</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
   </section>
 </template>
 <script>
--- src/views/game.details.vue
+++ src/views/game.details.vue
@@ -1,6 +1,6 @@
 <template>
   <section v-if="game" class="game-details">
-    <div class="details-background">
+    <div v-if="game.screenshots[0].image" class="details-background">
       <img :src="game.screenshots[0].image" alt="">
     </div>
     <div class="details-content container">
