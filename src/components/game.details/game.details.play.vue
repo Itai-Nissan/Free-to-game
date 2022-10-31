cd58e87725363c5d3a98dbf9efde57d2675aa929
Itai Nissan <itainissan@gmail.com>
2022-10-31 14:00:49 +0200

    details add to list button

--- src/components/game.details/game.details.play.vue
+++ src/components/game.details/game.details.play.vue
@@ -2,7 +2,8 @@
   <section v-if="game" class="game-details-play">
     <img v-bind:src="game.thumbnail" alt="">
     <div class="play-now-btn">
-      <button class="btn-basic-4">FREE</button>
+      <button @click="onAddToList" class="btn-basic-4">Add to list</button>
+      <!-- <button class="btn-basic-4">FREE</button> -->
       <a class="btn-basic" :href="game.game_url">PLAY NOW</a>
     </div>
     <!-- <div class="play-rate">
@@ -16,6 +17,8 @@
 </template>
     
 <script>
+import { showErrorMsg, showSuccessMsg } from '../../services/event-bus-service'
+
 export default {
   name: 'game-details-play',
   props: {
@@ -32,6 +35,21 @@ export default {
   mounted() {
   },
   methods: {
+    onAddToList() {
+      if (!this.user) {
+        showErrorMsg('Please log in')
+        return
+      }
+      else {
+        this.$store.dispatch('addToList', this.game)
+        showSuccessMsg(`${this.game.title} added to your list`)
+      }
+    },
+  },
+  computed: {
+    user() {
+      return this.$store.getters.loggedInUser
+    },
   },
 }
 </script>
