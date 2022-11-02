26c942520edab6b950466c4b272200d47ed4f2f2
Itai-Nissan <106192009+Itai-Nissan@users.noreply.github.com>
2022-11-02 12:36:54 +0200

    final build test

---
+++ assets/about.page.b71f071f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as _}from"./index.ea7f58c0.js";const a={},s={class:"about container"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",s,n)}const l=e(a,[["render",r]]);export{l as default};
