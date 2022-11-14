c599ac562a0da5a190d0be17bbb78a962efb73f8
Itai-Nissan <106192009+Itai-Nissan@users.noreply.github.com>
2022-11-14 13:17:43 +0200

    Delete assets directory

--- assets/learn.more.0435c27b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as _,a as s}from"./index.222f0d6f.js";const t={},n={class:"lear-more container"},a=s("h1",null,"This is an about page",-1),c=[a];function r(l,i){return o(),_("div",n,c)}const m=e(t,[["render",r]]);export{m as default};
